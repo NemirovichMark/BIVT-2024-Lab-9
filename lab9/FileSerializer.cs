c2cb3ae3400eba142214a364c01111923097ddec
Vadim Shubin <vbmshbn@yandex.ru>
2025-04-18 19:42:10 +0300

    first

---
+++ lab9/FileSerializer.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9
+{
+    public abstract class FileSerializer : iFileManager{
+        private string _folderPath;
+        private string _filePath;
+        private string _extension;
+
+        public string FolderPath => _folderPath;
+        public string FilePath => _filePath;
+        public abstract string Extension{
+            get;
+        }
+
+        public void SelectFolder(string path){
+            _folderPath = path;
+            if (!Directory.Exists(_folderPath)){
+                Directory.CreateDirectory(_folderPath);
+            }
+        }
+
+        void SelectFile(string name){
+            _filePath = name;
+            string full_path = $"{_folderPath}/{_filePath}.{Extension}";
+            if (!File.Exists(_filePath)){
+                File.Create(_filePath);
+            }
+        }
+    }
+}
---
+++ lab9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Lab_7;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_9
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+   
+    }   
+}
+}
