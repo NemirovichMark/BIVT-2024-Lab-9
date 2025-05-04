fc1c13dc7bb6b212476117e87e209e5fa1bb4bf3
tania <tania@MacBook-Pro-Tana.local>
2025-05-04 14:58:16 +0300

    meow commit

---
+++ Lab_9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using Lab_7;
+using Lab_9;
+
+class Program
+{
+    private static Random _rand = new Random();
+    
+    static void Main()
+    {
+        
+    }
+}
