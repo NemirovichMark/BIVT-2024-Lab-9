f6a857f338f9a0077366ec4a6fa55e6a64899655
Vinermy <egorkosachev@vk.com>
2025-05-20 16:10:10 +0300

    removed unnecessary tests, created serializer classes

--- PurpleSerializer.cs
+++ PurpleSerializer.cs
@@ -2,38 +2,20 @@ using Lab_7;
 
 namespace Lab_9
 {
-    public class PurpleSerializer : FileSerializer
+    public abstract class PurpleSerializer : FileSerializer
     {
         public override string Extension => "txt";
 
-        void SerializePurple1<T>(T obj, string fileName) where T : class
-        {
-        }
-        void SerializePurple2SkiJumping<T>(T jumping, string fileName) where T : Purple_2.SkiJumping;
-        void SerializePurple3Skating<T>(T skating, string fileName) where T : Purple_3.Skating;
-        void SerializePurple4Group(Purple_4.Group group, string fileName);
-        void SerializePurple5Report(Purple_5.Report report, string fileName);
+        public abstract void SerializePurple1<T>(T obj, string fileName) where T : class;
+        public abstract void SerializePurple2SkiJumping<T>(T jumping, string fileName) where T : Purple_2.SkiJumping;
+        public abstract void SerializePurple3Skating<T>(T skating, string fileName) where T : Purple_3.Skating;
+        public abstract void SerializePurple4Group(Purple_4.Group group, string fileName);
+        public abstract void SerializePurple5Report(Purple_5.Report report, string fileName);
 
-        void SerializePurple1Participant(Purple_1.Participant participant, string fileName)
-        {
-            int[] marksArray = new int[participant.Marks.GetLength(0) * participant.Marks.GetLength(1)];
-            for (int i = 0; i < participant.Marks.GetLength(1); i++)
-            {
-                for (int j = 0; j < participant.Marks.GetLength(0); j++)
-                {
-                    marksArray[i * participant.Marks.GetLength(0) + j] = participant.Marks[i, j];
-                }
-            }
-            string[] lines = {
-            $"name: {participant.Name}",
-            $"surname: {participant.Surname}",
-            $"marks: {string.Join(',', marksArray)}",
-            $"coefs: {string.Join(',', participant.Coefs)}",
-          };
-
-            int fileNameStartAfter = fileName.LastIndexOf('/');
-            string filePath = fileName.Take(fileNameStartAfter).ToString();
-            fileName = fileName.TakeLast(fileName.Length)
-        }
+        public abstract T DeserializePurple1<T>(string fileName) where T : class;
+        public abstract T DeserializePurple2SkiJumping<T>(string fileName) where T : Purple_2.SkiJumping;
+        public abstract T DeserializePurple3Skating<T>(string fileName) where T : Purple_3.Skating;
+        public abstract Purple_4.Group DeserializePurple4Group(string fileName);
+        public abstract Purple_5.Report DeserializePurple5Report(string fileName);
     }
 }
---
+++ PurpleXMLSerializer.cs
@@ -0,0 +1,57 @@
+using Lab_7;
+
+namespace Lab_9
+{
+    public class PurpleXMLSerializer : PurpleSerializer
+    {
+        public override T DeserializePurple1<T>(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T DeserializePurple2SkiJumping<T>(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T DeserializePurple3Skating<T>(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Purple_4.Group DeserializePurple4Group(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Purple_5.Report DeserializePurple5Report(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SerializePurple1<T>(T obj, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SerializePurple2SkiJumping<T>(T jumping, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SerializePurple3Skating<T>(T skating, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SerializePurple4Group(Purple_4.Group group, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SerializePurple5Report(Purple_5.Report report, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
